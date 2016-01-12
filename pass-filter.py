f8b19c71b23bf288745b96f87a785f4c74f2c7ea
Przemek Kaminski <przemyslaw.kaminski@sensisoft.com>
2016-01-12 21:29:07 +0100

    Initial commit -- pass filter works

---
+++ pass-filter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import fnmatch
+import os
+import sys
+
+
+QUERY = sys.argv[1]
+HOME = os.environ['HOME']
+PASS_DIR = os.environ.get('PASSWORD_STORE_DIR', os.path.join(HOME, '.password-store/'))
+
+
+# TODO: list_passwords creates cache of passwords for first time
+def list_passwords():
+    ret = []
+
+    for root, dirnames, filenames in os.walk(PASS_DIR):
+        for filename in fnmatch.filter(filenames, '*.gpg'):
+            ret.append(os.path.join(root, filename).rstrip('.gpg').replace(PASS_DIR, ''))
+
+    return ret
+
+
+def search_passwords(query):
+    ret = []
+
+    terms = filter(lambda x: x, query.split())
+    passwords = list_passwords()
+
+    for password in passwords:
+        for t in terms:
+            if t not in password:
+                break
+        else:
+            ret.append(password)
+
+    return ret
+
+
+def xmlize_items(items):
+    items_a = []
+
+    for item in items:
+        items_a.append("""
+    <item arg="%(item)s">
+        <title>%(item)s</title>
+    </item>
+        """ % {'item': item})
+
+    return """
+<?xml version="1.0"?>
+<items>
+    %s
+</items>
+    """ % '\n'.join(items_a)
+
+
+items = search_passwords(QUERY)
+print xmlize_items(items)
+
---
+++ pass-show.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+QUERY=$1
+PATH=/usr/local/bin:$PATH
+
+# GPG agent
+envfile="$HOME/.gnupg/gpg-agent.env"
+if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+    eval "$(cat "$envfile"); export GPG_AGENT_INFO"
+else
+    eval "$(gpg-agent --daemon --write-env-file "$envfile")"
+fi
+
+# PASS
+pass show $QUERY | awk 'BEGIN{ORS=""} {print; exit}' | pbcopy
